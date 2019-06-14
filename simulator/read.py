4a3990ed6209503053326ea568a6d538655714d8
Michael Li <minghao.li1@ucalgary.ca>
2019-06-13 18:14:44 -0600

    Restructure read and simulator into simulator dir
    Read contains two classes: Read and ReadPair.
    - ReadPair holds information on start/end of reads, the strand, and
      methylation calls within the read pairs. Can also create Read objects
      from each read.
    - Read holds information on the chromosome, the strand, start/end,
      methylation calls, and the sequence on a single sequencing read. Can
      also create a FASTQ entry for the read.

---
+++ simulator/read.py
@@ -0,0 +1,167 @@
+# read.py
+from typing import Dict, List, NoReturn, Tuple, Union
+
+from Bio.Seq import Seq, MutableSeq
+
+import utils.util as util
+
+
+class Read:
+    """
+    TODO: [DOCSTRING]:: complete this
+    A sequence read.
+
+    Attributes
+    ----------
+    chrom : str
+
+    strand : int
+
+    start_end : tuple of int
+
+    met_calls : dict of {int, int}
+
+    seq : Bio.Seq.MutableSeq
+
+    Methods
+    -------
+    fastq_entry()
+
+    """
+    def __init__(
+        self,
+        chrom: str,
+        strand: int,
+        start_end: Tuple[int],
+        met_calls: Dict[int, int],
+        seq: MutableSeq,
+    ) -> None:
+        self.chrom: str = chrom
+        self.strand: int = strand
+        self.start: int = start_end[0]
+        self.end: int = start_end[1]
+        self.seq: MutableSeq = seq
+
+    def fastq_entry(self) -> str:
+        """
+        Creates a FASTQ entry for the read.
+
+        Returns
+        -------
+        str
+            FASTQ entry for the read in this format:
+            @ Read name
+            Sequence
+            +
+            Quality
+        """
+        self._update_seq_met()
+        return "{}\n{}\n+\n{}\n".format(self._read_name(), self._read_qual(), self.seq)
+
+    def _update_seq_met(self) -> None:
+        """
+        Updates read sequence methylation states based on methylation calls.
+        """
+        met_states: List[chr] = ["T", "C"]
+        for k, v in self.met_calls.iteritems():
+            self.seq[k] = met_states[v]
+
+        # Reverse the complemented strand so that it's 5' -> 3'.
+        if self.strand:
+            self.seq.reverse()
+
+    def _read_name(self) -> str:
+        """
+        Creates the read name from the chromosome, the strand, and the start and end positions.
+        Note that start and end positions are based on forward strand but the sequence will be
+        5' -> 3' for reverse strands.
+
+        Returns
+        -------
+        str
+            The read name in the following format:
+            @[chromosome], [direction] strand: [start]-[end]
+        """
+        strands: List[str] = ["forward", "reverse"]
+        return "@{}, {} strand: {}-{}".format(
+            self.chrom, strands[self.strand], self.start, self.end
+        )
+
+    def _read_qual(self) -> str:
+        """
+        Creates the read quality string (PHRED33). Currently set to 40.
+        TODO: [FEATURE]:: Make quality user-specifiable.
+
+        Returns
+        -------
+        str
+            The read quality string.
+        """
+        return "I" * len(self.seq)
+
+
+class ReadPair:
+    """
+    A pair of paired end reads on the same strand.
+
+    Attributes
+    ----------
+    strand : int
+        The strand the reads are on. 0 for forward, 1 for reverse.
+
+    reads : tuple of tuple of int
+        A tuple of the read start-end position tuples.
+
+    read_met_calls : tuple of dict of {int, int}
+        Methylation states at methylation sites within each of the reads.
+
+    Methods
+    -------
+    create_read()
+        Create a Read object for a specified read in the read pair.
+    """
+
+    def __init__(
+        self,
+        strand: int,
+        reads: Tuple[Tuple[int, int], Tuple[int, int]],
+        read_met_calls: Tuple[Dict[int, int], Dict[int, int]],
+    ) -> None:
+        self.strand: int = strand
+        self.reads: Tuple[Tuple[int, int], Tuple[int, int]] = reads
+        self.read_met_calls: Tuple[Dict[int, int], Dict[int, int]] = read_met_calls
+
+    def create_read(self, chrom: str, read: int, chrom_seq: Seq) -> Union[NoReturn, Read]:
+        """
+        Create a Read objected from the specified read in the read pair.
+
+        Parameters
+        ----------
+        chrom : str
+            The chromosome the read pair is on.
+
+        read : int
+            Which read in the pair to create the Read object from. 0 or 1 only.
+
+        chrom_seq : Bio.Seq.Seq
+            The sequence of the chromosome.
+
+        Returns
+        -------
+        Read
+            Read object.
+
+        Throws
+        ------
+        ValueError
+            Read param isn't 0 or 1.
+        """
+        if read not in [0, 1]:
+            raise ValueError("Invalid read in pair specified.")
+
+        curr_read: Tuple[int, int] = self.reads[read]
+        read_seq: MutableSeq = chrom_seq[curr_read[0] : curr_read[1]].tomutable()
+        if self.strand:
+            read_seq.complement()
+
+        return Read(chrom, self.strand, curr_read, self.read_met_calls[read], read_seq)
---
+++ simulator/simulator.py
@@ -0,0 +1,166 @@
+# simulator.py
+
+import csv
+import random
+import time
+from pathlib import Path
+from typing import Dict, List, Optional, Tuple
+
+from Bio.Seq import Seq
+from Bio.SeqIO.QualityIO import FastqGeneralIterator
+from numpy.random import normal
+
+from .read import ReadPair, Read
+import utils.util as util
+
+
+class Simulator:
+    """
+    Simulate given number of reads for a given chromosome.
+
+    Attributes
+    ----------
+    chrom : str
+        The chromosome to simulate reads from.
+
+    seq : Bio.Seq.Seq
+        Biopython immutable sequence object; the DNA sequence of the current chromosome.
+
+    chrom_met_calls : tuple of dict of {int, int}
+        Methylation sites and states for each strand of the current chromosome.
+
+    num_reads : int
+        Number of read pairs to simulate.
+
+    read_len : int
+        Length of the simulated reads.
+
+    insert_mutupr designated mean insert size.
+
+    insert_sigma : int
+        User designated insert size standard deviation.
+
+    output_folder : pathlib.Path
+        Output directory path.
+
+    Methods
+    -------
+    sim_all_reads()
+        Simulate all read pairs.
+    """
+    def __init__(
+        self,
+        chrom: str,
+        seq: Seq,
+        chrom_met_calls: Tuple[Dict[int, int]],
+        num_reads: int,
+        read_len: int,
+        insert_mu: int,
+        insert_sigma: int,
+        output_folder: Path,
+    ) -> None:
+        # Chromosome information.
+        self.chrom = chrom
+        self.sequence: str = seq
+        self.seq_len: int = len(seq) - 1  # 0-based
+
+        # Methylation calls.
+        self.met_calls: Tuple[Dict[int, int]] = chrom_met_calls
+
+        # Read simulation properties.
+        self.num_reads: int = num_reads
+        self.read_len: int = read_len
+        self.insert_mu: int = insert_mu
+        self.insert_sigma: int = insert_sigma
+
+        # Output file paths.
+        self.fq_1: Path = output_folder.joinpath("sim_reads_1.fq")
+        self.fq_2: Path = output_folder.joinpath("sim_reads_2.fq")
+
+    def sim_all_reads(self) -> None:
+        """Simulate all read pairs and write to file."""
+        simmed_reads: int = 0  # accumulator
+        while simmed_reads <= self.num_reads:
+            curr_read_pair: ReadPair = self._sim_read()  # sim read pair
+
+            # If pair is discarded, continue loop without updating accumulator.
+            if curr_read_pair is None:
+                continue
+
+            # Create Read objects for each read in the pair.
+            read1: Read = curr_read_pair.create_read(self.chrom, 0, self.sequence)
+            read2: Read = curr_read_pair.create_read(self.chrom, 1, self.sequence)
+
+            # Write read entry to output fastq file.
+            with open(self.fq_1, "a") as f1, open(self.fq_2, "a") as f2:
+                f1.write(read1.fastq_entry())
+                f2.write(read2.fastq_entry())
+
+            simmed_reads += 1  # update accumulator
+
+    def _sim_read(self) -> Optional[ReadPair]:
+        """
+        Simulate a pair of paired end reads.
+
+        Returns
+        -------
+        read.PairedRead or None
+            A PairedRead if the read coordinates remain within chromosomal boundaires. None
+            otherwise.
+        """
+        random.seed(time.time())  # seed current time
+
+        # Simulate read properties.
+        strand: int = random.randint(0, 1)
+        read1_start: int = random.randint(0, self.length)
+        insert_size: int = round(normal(self.insert_mu, self.insert_sigma, 1)[0])
+
+        # Set direction as per the strand.
+        directed_read_len: int = -self.read_len if strand else self.read_len
+        directed_insert_size: int = -insert_size if strand else insert_size
+
+        # Early guard against chromosomal boundaries. Discard if bounds are violated.
+        read2_end: int = read1_start + directed_insert_size + (2 * directed_read_len)
+        if read2_end < 0 or read2_end > (self.seq_len + 1):
+            return None
+
+        # TODO: [TESTING]:: assert values are as expected
+
+        # Set rest of read values.
+        reads: Tuple[Tuple[int, int], Tuple[int, int]] = (
+            (read1_start, read1_start + directed_read_len),
+            (read2_end - directed_read_len, read2_end),
+        )
+
+        # Ensure ascending positional order (for start and end) for reverse strand.
+        if strand:
+            reads = (util.reverse_tuple_pair(reads[0]), util.reverse_tuple_pair(reads[1]))
+
+        # Extract subset of methylation calls.
+        read_met_calls: Dict[int, int] = self._subset_met_calls(strand, reads)
+
+        return ReadPair(strand, reads, read_met_calls)
+
+    def _subset_met_calls(
+        self, strand: int, reads: Tuple[Tuple[int, int], Tuple[int, int]]
+    ) -> Tuple[Dict[int, int], Dict[int, int]]:
+        """
+        Discover the subset of methylation calls within a pair of paired end reads.
+
+        Parameters
+        ----------
+        strand : int
+            The strand the read pair is on. 0 for forward, 1 for reverse.
+
+        reads : tuple of tuple of int
+            A tuple of the read start-end position tuples.
+
+        Returns
+        -------
+        tuple of dict of {int, int}
+            Dictionaries of position to methylation state of all positions within the paired reads.
+        """
+        return (
+            util.subset_dict(reads[0], self.met_calls[strand]),
+            util.subset_dict(reads[1], self.met_calls[strand]),
+        )
